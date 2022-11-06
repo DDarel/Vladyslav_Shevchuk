4f57462f87a294096d4ab163cb167f1f9f5dd85b
Vladyslav Shevchuk <darel.dahanavar.shevchyk@gmail.com>
2022-11-06 18:39:54 +0200

    feat: Added ExtraTasks files

---
+++ TasksAndTests/ExtraTask1+Tests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace TasksAndTests
+{
+    public class ExtraTask1Tests
+    {
+        public static int ReturnBiggerNumber(int number)
+        {
+            int[] arrNumber = new int[number.ToString().Length];
+            int tracker = 0;
+            while (number > 0) {
+                arrNumber[tracker] = number % 10;
+                number/=10;
+                tracker++;
+            }
+            //2347 ---> [7][4][3][2] ---> [4][7][3][2] ---> 2374
+            //2343 ---> [3][4][3][2] ---> [3][3][4][2] ---> 2433
+            //1231  ---> [1][3][2][1]  ---> [2][1][3][1]  ---> 1312
+            tracker = 0;
+            while (tracker < arrNumber.Length - 1) {
+                if (arrNumber[tracker] > arrNumber[tracker + 1])
+                {
+                    int temp = arrNumber[tracker];
+                    arrNumber[tracker] = arrNumber[tracker + 1];
+                    arrNumber[tracker + 1] = temp;
+                    while (tracker > 0)
+                    {
+                        if (arrNumber[tracker] > arrNumber[tracker - 1]) {
+                            temp = arrNumber[tracker];
+                            arrNumber[tracker] = arrNumber[tracker - 1];
+                            arrNumber[tracker - 1] = temp;
+                        } 
+                        tracker--; 
+                    }
+                    break;
+                }
+                else tracker++;
+            }
+            number = 0;
+            for (int i = 0; i < arrNumber.Length; i++) {
+                number = number + arrNumber[i] * (int)(Math.Pow(10, i));
+            }
+            return number;
+        }
+
+
+        [Test]
+        public void ExtraTask1Test1()
+        {
+            //Arrange
+            int input = 2047;
+            int expected = 2074;
+
+            //Act
+            input = ReturnBiggerNumber(input);
+
+            //Assert
+            Assert.AreEqual(expected, input);
+        }
+        [Test]
+        public void ExtraTask1Test2()
+        {
+            //Arrange
+            int input = 1231;
+            int expected = 1312;
+
+            //Act
+            input = ReturnBiggerNumber(input);
+
+            //Assert
+            Assert.AreEqual(expected, input);
+        }
+    }
+}
---
+++ TasksAndTests/ExtraTask2+Tests.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Security.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TasksAndTests
+{
+    public class ExtraTask2Tests
+    {
+        public static string uintToAdress(uint num)
+        {
+            int  tracker = 0;
+            uint[] binArray = new uint[32];
+            while (tracker < 32)
+            {
+                binArray[tracker] = (num % 2);
+                num /= 2;
+                tracker++;
+            }
+            tracker = 0;
+            string output = "";
+            while (tracker < 4) {
+                uint dec = 0;
+                int pow = 0;
+                for (int j= tracker * 8; j < tracker * 8 + 8; j++) {
+                    dec += binArray[j] * (uint)Math.Pow(2, pow);
+                    pow++;
+                }
+                output = output.Insert(0, dec.ToString());
+                if (tracker != 3) {
+                    output = output.Insert(0, ".");
+                }
+                tracker++;
+            }
+            return output;
+        }
+
+        [Test]
+        public void ExtraTask2Test1()
+        {
+            //Arrange
+            uint input = 2149583361;
+            string expected= "128.32.10.1";
+
+            //Act
+            string result = uintToAdress(input);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+        [Test]
+        public void ExtraTask2Test2()
+        {
+            //Arrange
+            uint input = 32;
+            string expected = "0.0.0.32";
+
+            //Act
+            string result = uintToAdress(input);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
